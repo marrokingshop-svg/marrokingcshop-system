9b003ad25d4b72caa1b6f06ee909c1ab0619f34d
marrokingshop-svg <marroquincshop@gmail.com>
2026-02-19 03:36:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -29,10 +29,12 @@ security = HTTPBearer()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://marrokingshop-svg.github.io",
-        "http://localhost:8000",
-        "http://127.0.0.1:8000"
+    allow_origins=["*"],  # El asterisco permite cualquier origen y suele saltar el error de caché
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"]
+)
     ],
     allow_credentials=True,
     allow_methods=["*"],
