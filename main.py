01833fb88eecbd3f9a217aa34d91580e0669cd67
marrokingshop-svg <marroquincshop@gmail.com>
2026-02-19 03:06:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -29,10 +29,15 @@ security = HTTPBearer()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://marrokingshop-svg.github.io",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"]
 )
 
 # =====================================================
